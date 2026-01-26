a2d99ff5081c199791cd010771baf9cbe4db5583
jona159 <raabejonas@yahoo.de>
2026-01-26 11:12:15 +0100

    fix: allow creators to invite partners

--- src/app/issuer/components/network-dashboard/network-dashboard.component.ts
+++ src/app/issuer/components/network-dashboard/network-dashboard.component.ts
@@ -175,7 +175,8 @@ export class NetworkDashboardComponent extends BaseAuthenticatedRoutableComponen
 	private readonly _hlmDialogService = inject(HlmDialogService);
 
 	public openDialog() {
-		if (this.network().current_user_network_role != 'owner') return;
+		const role = this.network().current_user_network_role;
+		if (!['owner', 'creator'].includes(role)) return;
 		const dialogRef = this._hlmDialogService.open(DialogComponent, {
 			context: {
 				headerTemplate: this.headerTemplate(),
